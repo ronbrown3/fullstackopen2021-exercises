f9bbf37cc65da5470b36cc17803518ea422eb679
ronbrown3 <73085072+ronbrown3@users.noreply.github.com>
2022-01-16 00:17:30 -0600

    Add exercise solution 3.8

---
+++ part3/phonebook/index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(express.json());
+
+morgan.token('content', (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :content'
+  )
+);
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Persons API</h1>');
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/info', (request, response) => {
+  response.send(
+    `Phonebook has info for ${persons.length} people <br /><br /> ${Date()}`
+  );
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.json({ error: 'Name or number is missing' });
+  } else if (persons.find((person) => person.name === Number(body.name))) {
+    return response.json({ error: 'Name already exists in phonebook' });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
